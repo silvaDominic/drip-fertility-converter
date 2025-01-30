461eda72247ca01b92e9aea44238d7ca4fc20700
Dominic <string.reverse@proton.me>
2025-01-29 21:44:38 -0500

    Refactored RYB mappings

--- src/RYB/ryb.js
+++ src/RYB/ryb.js
@@ -1,4 +1,4 @@
-const SEX_FIELDS = {
+const CONTRACEPTIVES_MAP = {
   "solo": "solo",
   "unprotected": "other",
   "condom": "condom",
@@ -20,9 +20,9 @@ const BLEEDING_MAP = {
 }
 
 const CERVIX_FIRMNESS_MAP = {
-  'soft': 0,
-  'medium': null, // Note
-  'firm': 2,
+  'firm': 0,
+  'medium': null,
+  'soft': 1,
 }
 
 const CERVIX_POSITION_MAP = {
@@ -51,21 +51,54 @@ const CERVIX_MUCUS_MAP = {
 }
 
 export function mapRYB(jsonData) {
-  console.log(jsonData);
-  let mappedData = [];
-  jsonData.data.forEach(entry => {
-    const date = new Date(entry['date']).toISOString().split('T')[0];
-    const bleedingVal = getBleedingValue(entry['fluid']);
-    const temp = entry['tempOneValue'];
-    const note = entry['note'];
-    const cervixOpening =CERVIX_OPENING_MAP[entry['cervixOpenness']]
-    const cervixFirmness = CERVIX_FIRMNESS_MAP[entry['cervixFirmness']] || null;
-    const cervixPosition = CERVIX_POSITION_MAP[entry['cervixHeight']] || null;
-    const cervixMucus = getCervicalFluidVal(entry['fluid']);
-    const cervixFeeling = VAGINAL_SENSATION_MAP[entry['sensation']] || 0; // Default to 0 (nothing)
-    const cervixTilt = "" // Note
-    const intercourseType = getSexType(entry["intercourse"]);
+  return jsonData.data.map(entry => {
+    let notes = "";
+    const dripEntry = {
+      ['date']: new Date(entry['date']).toISOString().split('T')[0],
+      ['temperature.value']: entry['tempOneValue'] || "",
+      ['temperature.exclude']: false,
+      ['bleeding.value']: getBleedingValue(entry['fluid']),
+      ['bleeding.exclude']: !!getBleedingValue(entry['fluid']) ? false : null,
+      ['mucus.value']: getCervicalFluidVal(entry['fluid']),
+      ['mucus.feeling']: VAGINAL_SENSATION_MAP[entry['sensation']] || 0, // Default to 0 (nothing)
+      ['mucus.exclude']: false,
+      ['cervix.opening']: CERVIX_OPENING_MAP[entry['cervixOpenness']] || null,
+      ['cervix.firmness']: CERVIX_FIRMNESS_MAP[entry['cervixFirmness']] ?? null,
+      ['cervix.position']: CERVIX_POSITION_MAP[entry['cervixHeight']] || null,
+      ['cervix.exclude']: false,
+    }
+
+    // Set sex type (solo/partner)
+    const sexType = getSexType(entry["intercourse"]);
+    if (sexType) {
+      dripEntry[`sex.${ sexType }`] = true;
+    }
+
+    // Set various contraceptive types
     const contraceptiveTypes = getContraceptiveTypes(entry["intercourse"]);
+    contraceptiveTypes.forEach(item => {
+      if (item.asNote) {
+        notes += `Sex: ${ item.name } \n`; // Add unsupported types as notes
+      } else if (item.name) {
+        dripEntry[`sex.${ item.name }`] = true;
+      }
+    });
+
+    // Add unsupported values as notes
+    if (entry['cervixFirmness'] === 'medium') {
+      notes += `Cervix Firmness: ${ entry['cervixFirmness'] }\n`;
+    }
+    if (entry['cervixTilt']) {
+      notes += `Cervix Tilt: ${ entry['cervixTilt'] }\n`;
+    }
+
+    // Add any existing notes
+    if (entry['notes']) {
+      notes += `${ entry['note'] }`;
+    }
+    dripEntry['note.value'] = notes;
+
+    return dripEntry;
   });
 }
 
@@ -78,24 +111,43 @@ export function getBleedingValue(values) {
 }
 
 export function getCervicalFluidVal(values) {
-  const tokens = values.split(',').map(token => token.trim()).filter((token => Object.keys(CERVIX_MUCUS_MAP).includes(token)));
+  const tokens = values
+    .split(',')
+    .map(token => token.trim())
+    .filter((token => Object.keys(CERVIX_MUCUS_MAP).includes(token)));
   // Sort mucus items based on the defined priority
   const sortedItems = tokens.sort((a, b) => (CERVIX_MUCUS_MAP[b] - CERVIX_MUCUS_MAP[a]));
   // Return the highest-priority item (first in the sorted list)
-  return sortedItems[0] || null;  // Return null if no items found
+  return CERVIX_MUCUS_MAP[sortedItems[0]] || null; // Return null if no items found
 }
 
 export function getSexType(values) {
-  const tokens = values.split(',').map(token => token.trim());
-    if (tokens.includes(SEX_FIELDS["solo"])) return "solo";
-    for (const token of tokens) {
-      if (tokens.includes(token)) return "partner";
-    }
-  return null;
+  if (values.trim() === "") return null;
+
+  const types = values
+    .split(',')
+    .map(token => token.trim());
+
+  // If solo is found at all, assume 'solo' sex.
+  if (types.includes(CONTRACEPTIVES_MAP["solo"])) return "solo";
+  // RYB doesn't have a 'partner' term and so any sex method that isn't 'solo' is assumed as such.
+  for (const type of types) {
+    if (types.includes(type)) return "partner";
+  }
 }
 
-// TODO Evolve this to return obejcts with notes for 'other' values
 export function getContraceptiveTypes(values) {
-  const tokens = values.split(',').map(token => token.trim()).filter(token => token !== 'solo');
-  return tokens.map((token) => SEX_FIELDS[token]);
+  // Remove 'solo' type
+  const validTypes = values.split(',')
+    .map(token => token.trim())
+    .filter(token => token !== 'solo');
+
+  // Marks unsupported types to be a note and uses RYB name, otherwise, maps to associated Drip name
+  return validTypes.map((type) => {
+    if (CONTRACEPTIVES_MAP[type] === 'other') {
+      return { name: type, asNote: true }
+    } else {
+      return { name: CONTRACEPTIVES_MAP[type], asNote: false }
+    }
+  });
 }

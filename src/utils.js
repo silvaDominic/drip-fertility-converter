2b377d616a9bea1229bd9f22bf39f68d1c0d97f3
Dominic <string.reverse@proton.me>
2025-02-10 16:04:02 -0500

    Added type check for 'F to C' util

--- src/utils.js
+++ src/utils.js
@@ -9,6 +9,8 @@ export function convertCVStoJSON(csvData) {
 }
 
 export function fahrenheitToCelsius(temp) {
+  if (typeof temp !== 'number' || isNaN(temp)) return null;
+
   let celsius = (temp - 32) * (5 / 9);
   return parseFloat(celsius.toFixed(2)); // Round to 2 decimal points
 }
